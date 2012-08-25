33aa657d2e9f0288263045a3ef73dbf982ef9b3e
papanda <ichitanister@gmail.com>
2012-08-25 15:37:49 +0900

    jQueryMobile入れた

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,41 @@
+# coding: utf-8
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  layout :set_layout
+
+  before_filter :set_iphone_format, :japanese!, :reset_session_expires
+  helper_method :current_user
+
+  def set_iphone_format
+    request.format = :iphone if (iphone_request? || android_request?)
+  end
+
+  def set_layout
+    (iphone_request? || android_request?) ? "iphone" : "application"
+  end
+
+  private
+  def iphone_request?
+    request.user_agent =~ /(Mobile.+Safari)/
+  end
+  
+  def android_request?
+    request.user_agent =~ /(Android.*Mobile)/
+  end
+  
+  def japanese!
+    if (iphone_request? || android_request?)
+      I18n.locale = :ja
+      Time.zone = 'Asia/Tokyo'
+      response['Content-Language'] = 'content="ja"'
+    end
+  end
+  
+  def reset_session_expires
+  	request.session_options[:expire_after] = 120.minutes
+  end
+  
+  def current_user
+  	@current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
 end
